b6b152a766aac7ad381af01cbe8f2c7cfe799608
Denys Mogyliuk <denysmogyliuk@gmail.com>
2022-06-04 00:21:11 +0300

    delete strings with console.log

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ const getRandomNumber = (min, max) => {
   if(min >= 0 && max >= 0 && min < max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
-    console.log('Недопустимые значения аргументов функции');
     return NaN;
   }
 };
@@ -13,7 +12,6 @@ const getRandomNumberWithPoint = (min, max, fraction) => {
   if(min >= 0 && max >= 0 && min < max && fraction >= 0 && Number.isInteger(fraction)) {
     return ((Math.random() * (max - min + 1)) + min).toFixed(fraction);
   } else {
-    console.log('Недопустимые значения аргументов функции');
     return NaN;
   }
 };
