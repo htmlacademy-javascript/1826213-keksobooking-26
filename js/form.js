d3f03c28c917d6590bfc8c8ecf29bb60d35cac62
Denys Mogyliuk <denysmogyliuk@gmail.com>
2022-06-30 15:00:36 +0300

    add synchronization of checktimeses, validation of price by housing types

--- js/form.js
+++ js/form.js
@@ -1,11 +1,16 @@
+const MAX_HOUSING_PRICE = 100000;
 const formAd = document.querySelector('.ad-form');
 const formFilter = document.querySelector('.map__filters');
 const formAdInteractiveElements = formAd.querySelectorAll('fieldset');
 const formFilterInteractiveElements = formFilter.querySelectorAll(['select', 'fieldset']);
 // const formTitleInput = formAd.querySelector('#title');
-// const formPriceInput = formAd.querySelector('#price');
-const formRoomNumberInput = formAd.querySelector('[name="rooms"]');
-const formRoomCapacityInput = formAd.querySelector('[name="capacity"]');
+const formPriceInput = formAd.querySelector('#price');
+const formRoomNumberInput = formAd.querySelector('#room_number');
+const formRoomCapacityInput = formAd.querySelector('#capacity');
+const formHousingTypes = formAd.querySelector('#type');
+const formCheckIn = formAd.querySelector('#timein');
+const formCheckOut = formAd.querySelector('#timeout');
+const formTimeParent = formAd.querySelector('.ad-form__element--time');
 
 const ROOMS_CAPACITY = {
   '1': ['1'],
@@ -14,26 +19,27 @@ const ROOMS_CAPACITY = {
   '100': ['0'],
 };
 
-const pristine = new Pristine(formAd, {
-  classTo: 'ad-form__element',
-  errorTextParent: 'ad-form__element',
-  errorTextClass: 'ad-form__error-text',
+const LIVING_PRICES = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000,
+};
+
+Pristine.addMessages('ru', {
+  required: 'Это поле должно быть заполнено',
+  maxlength: `Должно быть не более \${${1}} символов`,
+  minlength: `Должно быть не менее \${${1}} символов`,
 });
 
-const validateCapacity = (value) => ROOMS_CAPACITY[formRoomNumberInput.value].includes(value);
+Pristine.setLocale('ru');
 
-pristine.addValidator(formRoomCapacityInput, validateCapacity, 'Выберите верное количество комнат и гостей');
+// Function to get minimal price
+const getMinPrice = () => LIVING_PRICES[formHousingTypes.value];
 
-formAd.addEventListener('change', () => {
-  pristine.validate(formRoomCapacityInput);
-});
 
-formAd.addEventListener('submit', (evt) => {
-  if(!pristine.validate()) {
-    evt.preventDefault();
-  }
-});
-
+//  Form to disable
 const setDisabledCondition = () => {
   formAd.classList.add('ad-form--disabled');
   formFilter.classList.add('ad-form--disabled');
@@ -46,6 +52,8 @@ const setDisabledCondition = () => {
   });
 };
 
+
+// Form to enable
 const setEnabledCondition = () => {
   formAd.classList.remove('ad-form--disabled');
   formFilter.classList.remove('ad-form--disabled');
@@ -56,6 +64,56 @@ const setEnabledCondition = () => {
   formFilterInteractiveElements.forEach((element) => {
     element.removeAttribute('disabled');
   });
+
+  formPriceInput.placeholder = getMinPrice();
+  formPriceInput.min = getMinPrice();
 };
+
+setEnabledCondition();
+
+const pristine = new Pristine(formAd, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__error-text',
+});
+
+
+// Set minimal price in placeholder and min attribute
+formHousingTypes.addEventListener('change', () => {
+  formPriceInput.placeholder = getMinPrice();
+  formPriceInput.min = getMinPrice();
+});
+
+
+// Rooms validation
+const validateCapacity = (value) => ROOMS_CAPACITY[formRoomNumberInput.value].includes(value);
+pristine.addValidator(formRoomCapacityInput, validateCapacity, 'Выберите верное количество комнат и гостей');
+formAd.addEventListener('change', () => {
+  pristine.validate(formRoomCapacityInput);
+});
+
+
+// Price validation
+const validatePrice = (value) => value >= getMinPrice() && value <= MAX_HOUSING_PRICE;
+const getPriceErrorMessage = () => `Не менее ${getMinPrice()} и не более 100000`;
+pristine.addValidator(formPriceInput, validatePrice, getPriceErrorMessage);
+
+formAd.addEventListener('input', () => {
+  pristine.validate(formPriceInput);
+});
+
+
+// Checkin and checkout synchronization
+formTimeParent.addEventListener('change', (evt) => {
+  formCheckOut.value = evt.target.value;
+  formCheckIn.value = evt.target.value;
+});
+
+
+formAd.addEventListener('submit', (evt) => {
+  if(!pristine.validate()) {
+    evt.preventDefault();
+  }
+});
 
 export {setDisabledCondition, setEnabledCondition};
